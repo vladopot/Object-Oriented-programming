def65fdbdbad7c64c7b3f8220703c3c32b4ae806
vladsopot <96959552+vladopot@users.noreply.github.com>
2024-10-24 11:26:47 +0200

    Add files via upload

---
+++ Laboratorium1/Laboratorium1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Programm 
+{
+    public static void Main()
+    {
+        //Ex1 firstEx = new Ex1();
+        //Console.WriteLine(firstEx.isEven(int.Parse(Console.ReadLine())));
+        //Ex2 secondEx = new Ex2();
+        //secondEx.Nums(int.Parse(Console.ReadLine()));
+        Ex3 tjitd = new Ex3();
+        tjitd.ShowMenu();
+    }
+}
+
+class Ex1
+{
+    public bool isEven(int num)
+    {
+        return num % 2 == 0;
+    }
+}
+
+class Ex2
+{
+    public void Nums(int num)
+    {
+        int currentNum = 1;
+        while (currentNum <= num)
+        {
+            Console.WriteLine(currentNum);
+            currentNum++;
+        }
+    }
+}
+
+class Ex3
+{
+    public void ShowMenu()
+    {
+        List<string> menuVars = new List<string>()
+        {
+            "1: first Exersise",
+            "2: second Exersise",
+            "3: third Exersise",
+            "Exit"
+        };
+        foreach (string var in menuVars) {
+            Console.WriteLine(var);
+        }
+        Console.WriteLine("Choose your Exersice");
+        int exNum = int.Parse(Console.ReadLine());
+    }
+
+}
