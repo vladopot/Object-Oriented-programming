b7fcd803e94537ba249323e59f872e4eed0ecec5
vladsopot <96959552+vladopot@users.noreply.github.com>
2024-11-28 11:14:02 +0100

    Add files via upload

---
+++ Lab3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿class Programm
+{
+    public static void Main(String[] args)
+    {
+        List<Shape> Shapes = new List<Shape>();
+        Shapes.Add(new Rectangle());
+        Shapes.Add(new Triangle());
+        Shapes.Add(new Circle());
+
+        foreach (Shape shape in Shapes) 
+        {
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
+
+class Shape{
+    public int X;
+    public int Y;
+    public int Height;
+    public int Width;
+
+    public virtual string Draw()
+    {
+        return "HYYYYYYYYYYj";
+    }
+}
+
+class Rectangle : Shape
+{
+    public override string Draw()
+    {
+        return "This is Rectangle";
+    }
+}
+
+class Triangle : Shape
+{
+    public override string Draw()
+    {
+        return "This is Triangle";
+    }
+}
+
+class Circle : Shape
+{
+    public override string Draw()
+    {
+        return "This is Circle";
+    }
+}
